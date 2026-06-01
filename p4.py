9119efdd7894360bb60c9ac191dcf7776c019b6d
KAVYA-data-cpu <gkavya09742@gmail.com>
2026-06-01 22:21:51 +0530

    Initial commit

---
+++ p4.py
@@ -0,0 +1,7 @@
+import os
+#specifying the path
+directory_path='C://Users//Admin//OneDrive//Desktop'
+#listing all the dirctories
+contents=os.listdir(directory_path)
+for qi in contents:
+    print(qi)
